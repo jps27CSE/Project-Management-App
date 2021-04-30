301fcd065d99aee7eb668ba24cfaed00ad61cb28
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2021-04-30 10:58:43 +0600

    Dashboard

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className="dashboard container">
+                <div className="row">
+                    <div className="col s12 m6">
+                        <div className="col s12 m5 offset-m1">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
