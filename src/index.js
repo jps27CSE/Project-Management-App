336490fd7f04de724623e8a98eb318fb240af449
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-05-06 11:48:00 +0600

    Adding Dummy Data

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 const store = createStore(rootReducer)
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {}
+const initState = {
+  projects: [
+    { id: '1', title: 'Jack Pritom Soren Building a Project', content: 'blah blah blah' },
+    { id: '2', title: 'collect all the stars', content: 'blah blah blah' },
+    { id: '3', title: 'Testing application', content: 'blah blah blah' }
+  ]
+}
 
 const projectReducer = (state = initState, action) => {
   return state
