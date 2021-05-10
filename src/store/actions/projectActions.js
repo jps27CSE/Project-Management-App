abca8a2cad7a93e0f6c8c841f7a7208f8baf82a7
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2021-05-10 16:02:39 +0600

    Adding Data to Firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,17 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
-        dispatch({ type: "CREATE_PROJECT", project })
+        const firestore = getFirestore();
+        firestore.collection('project').add({
+            ...project,
+            authFirstName: 'jack',
+            authLastName: 'pritom',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: "CREATE_PROJECT", project })
+        }).catch((err) => {
+            dispatch({ type: "CREATE_PROJECT_ERROR", err })
+        })
+
     }
 }
